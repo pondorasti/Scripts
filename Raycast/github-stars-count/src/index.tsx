07fe77884e50f47738381d6061bcdcac7f28a0aa
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-01-31 17:14:27 -0800

    wip: github-stars-count

--- Raycast/github-stars-count/src/index.tsx
+++ Raycast/github-stars-count/src/index.tsx
@@ -1,20 +1,30 @@
-import { useEffect, useState } from "react";
+import { useState, useCallback } from "react";
 import { List, Icon, Color, ImageMask } from "@raycast/api";
+import debounce from "lodash.debounce"
 import stars, { IResponse } from "./utils/stars";
 
 export default function Command() {
+  const [isLoading, setIsLoading] = useState(false);
   const [response, setResponse] = useState<IResponse | null>(null);
 
-  useEffect(() => {
-    async function fetchStars() {
-      const res = await stars("pondorasti");
-      setResponse(res);
-    }
-    fetchStars();
-  }, []);
+  const debouncedSearch = useCallback(
+    debounce((newValue: string) => {
+      if (!isLoading) {
+        fetchStars(newValue);
+      }
+    }, 400),
+    []
+  )
+
+  async function fetchStars(username: string) {
+    setIsLoading(true);
+    const res = await stars(username);
+    setIsLoading(false);
+    setResponse(res);
+  }
 
   return (
-    <List searchBarPlaceholder="Search username" isLoading={!response}>
+    <List searchBarPlaceholder="Search username" onSearchTextChange={(text) => debouncedSearch(text)} isLoading={isLoading} >
       {response &&
         (response.message ? (
           <List.Item title={response.message} icon={{ source: Icon.ExclamationMark, tintColor: Color.Red }} />
@@ -23,6 +33,7 @@ export default function Command() {
             <List.Item title={`${response.stars} total stars`} icon={{ source: Icon.Star, tintColor: Color.Yellow }} />
             {response.repos.map(({ name, description, stargazers_count, owner }) => (
               <List.Item
+                key={name}
                 title={name}
                 subtitle={description}
                 accessoryTitle={`${stargazers_count}  ★`}
